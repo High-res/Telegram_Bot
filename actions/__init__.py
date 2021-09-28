bfdd7357cc54ff191784540f5b0d8cf3b800c502
Aidar Zhusipov <35493222+High-res@users.noreply.github.com>
2021-09-28 18:03:12 +0600

    Delete actions directory

--- actions/__init__.py
+++
@@ -1,9 +0,0 @@
-from .addLocation import addLocation
-from .addToDocx import addToDocx
-from .getAvans import getAvans, getAvanses
-from .getText import getText
-from .getUser import getUser, addUser
-from .getWorker import getWorkerss
-from .getZhaloba import getZhaloba
-from .sendDocx import send_docx
-from .getChallenge import getChallenge, addChallengeAction, getChallengeAction
