197590f26983b1c91fff5dffa99388dc38882924
Aidar Zhusipov <35493222+High-res@users.noreply.github.com>
2021-09-29 18:59:05 +0600

    Delete src directory

--- src/mainFunctions/sendMessage.py
+++
@@ -1,6 +0,0 @@
-from main import bot
-
-
-# Auto send message
-async def send_message(channel_id: int, text: str):
-    await bot.send_message(channel_id, text)
--- src/mainFunctions/tgUsers.py
+++
@@ -1,9 +0,0 @@
-from src.actions.getUser import getUser
-
-
-tg_id = []
-def tgUsers() :
-    tgUsers = getUser()
-    for i in range(len(tgUsers)) :
-        tg_id.append(tgUsers[i]['tg_id'])
-    return tg_id
