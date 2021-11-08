f158bf9d06ada3713895da332281455f606d0460
Aidar Zhusipov <35493222+High-res@users.noreply.github.com>
2021-11-08 18:09:41 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
+from pytz import utc
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import config
 
@@ -13,13 +15,16 @@ bot = Bot(config.MAIN_BOT_API, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+
 if __name__ == "__main__":
     from src.menu.menu import dp
-    from src.schedulers.botAutoSend import bot_auto_send_schedule_to_main
-    from src.schedulers.birthday import birthday_schedule
-    from src.schedulers.challenge import challenge_scheduler
-    loop.create_task(bot_auto_send_schedule_to_main())
-    loop.create_task(birthday_schedule())
-    loop.create_task(challenge_scheduler())
-    executor.start_polling(dp, skip_updates=True)
+    from src.schedulers.challenge import botScheduler
+    from src.schedulers.botAutoSend import bot_auto_send_schedule
+    from src.schedulers.birthday import if_send
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(botScheduler, 'interval', seconds=60)
+    scheduler.add_job(bot_auto_send_schedule, 'interval', seconds=60)
+    scheduler.add_job(if_send, 'interval', seconds=60)
+    scheduler.start()
+    executor.start_polling(dp, skip_updates=False)
         
